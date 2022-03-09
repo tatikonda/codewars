7a6bb553bf9b5c9c1472cc682caac66e21e935fb
tatikonda <tatikonda@live.com>
2022-03-08 22:37:42 -0800

    Adding

--- proper_fraction.py
+++ proper_fraction.py
@@ -1,15 +1,13 @@
 def proper_fractions(n):
-    if n == 1:
-        return 0
-    euler = n
-    for p in range(2, int(n**0.5) + 1):
-        if n % p == 0:
-            euler *= 1 - 1/p
-            while n % p == 0:
-                n //= p
-    if n > 1:
-        euler *= 1 - 1/n
-        print(euler)
-    return int(euler)
+    if n==1: return 0
+    phi = n
+    for p in range(2, int(n**.5)+1):
+        if n%p==0: # these p's will always be primes
+            phi -= phi/p
+            while n%p==0: # take out all the powers of this prime
+                n /= p
+    if n>1: # if n itself is a prime
+        phi -= phi/n
+    return int(phi)
 
 print(proper_fractions(6))
