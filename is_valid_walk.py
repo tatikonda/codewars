8cc49f37d82ac07f04786674983697be6ca8f6cc
tatikonda <tatikonda@live.com>
2022-03-24 22:28:01 -0700

    Adding

---
+++ is_valid_walk.py
@@ -0,0 +1,8 @@
+def is_valid_walk(walk):
+    if (walk.count('n') == walk.count('s') and 
+        walk.count('e') == walk.count('w') and
+        len(walk) == 10):
+            return True
+    return False
+
+print(is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e']))
--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,7 @@
 def two_sum(nums, t):
     for i, x in enumerate(nums):
-        print('i>',i)
         for j, y in enumerate(nums):
-            print('j>',j)
             if i != j and x + y == t:
                 return [i, j]
 
-print((two_sum([1,3,2], 4)))
+print((two_sum([1,3,3,2], 4)))
