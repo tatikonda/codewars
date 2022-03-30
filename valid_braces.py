5efb1cdfdf063fabdada803dbbab13964aa6bbba
tatikonda <tatikonda@live.com>
2022-03-29 22:49:05 -0700

    Adding

---
+++ valid_braces.py
@@ -0,0 +1,8 @@
+def valid_braces(s):
+  while '{}' in s or '()' in s or '[]' in s:
+      s=s.replace('{}','')
+      s=s.replace('[]','')
+      s=s.replace('()','')
+  return s==''
+
+print(valid_braces('[{()]'))
